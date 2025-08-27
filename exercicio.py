f8c18934b7ee327c010f7b6b77525c133f4b55a2
Lavínia Ferreira <souzlaviniasouza.@gmail.com>
2025-08-27 12:55:08 -0300

    exercicios2

---
+++ exercicio.py
@@ -0,0 +1,7 @@
+# 1. Fazer um programa que leia o nome de uma pessoa
+#  e mostre uma mensagem de boas-vindas.
+
+nome = input('Digite seu nome: ')
+
+print(f'Olá {nome}, seja bem vindo!')
+
---
+++ exercicio02.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = pow(numero, 0.5)
+
+print(f'O dobro é {dobro}')
+print(f'O triplo é {triplo}')
+print(f'A raiz quadrada é {raiz}')
